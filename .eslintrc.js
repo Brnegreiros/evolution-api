c3ef9eea61a7acf40df6943ad8df28c132a661c7
Bruno Negreiros <brnegreiros2@gmail.com>
2025-10-28 17:44:37 -0400

    fix(config): convert ESLint config to ES Module format
    - Update .eslintrc.js to use ES Module syntax
    - Add fileURLToPath and dirname imports for __dirname compatibility
    - Resolve ESLint configuration issues with ES Module project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
-module.exports = {
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
